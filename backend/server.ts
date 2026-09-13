c175719026aad07d015f46baf99cab3341892c8b
Ankit Nonia <studyankit2017@gmail.com>
2026-09-13 22:53:04 +0530

    Created backend server with express and connected with mongodb database

---
+++ backend/server.ts
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import express, { Request, Response } from 'express';
+import cors from "cors";
+import 'dotenv/config'
+import connectDb from "./configs/db.js";
+
+await connectDb();
+
+const app = express();
+
+// Middleware
+app.use(cors())
+app.use(express.json());
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Server is Live!');
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
